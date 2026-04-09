f05d7fe265e7bd4bd22063bfd5b5706fb1f21e54
MaximeHES <117347361+MaximeHES@users.noreply.github.com>
2026-04-09 09:37:46 +0200

    First stable version for loading cleaned data into the database
    loader for each category
    sql scripts
    including a testDB.py to check the connetion with your DB

---
+++ Gold_loading/db_config.py
@@ -0,0 +1,9 @@
+# db_config.py
+
+DB_CONFIG = {
+    "server": "gold-server10.database.windows.net",
+    "database": "Gold",
+    "username": "sqladmin",
+    "password": "AdminHevs01",
+    "driver": "ODBC Driver 17 for SQL Server"
+}
